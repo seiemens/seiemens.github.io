b9811f17d50411a74d357836546cd9311caf94ea
wadafacc <schlumpftimo14@gmail.com>
2023-02-27 14:12:53 +0100

    init base structure

---
+++ scripts/header.js
@@ -0,0 +1,9 @@
+function expand() {
+    let container = document.getElementById("header-container");
+    container.style.height = "fit-content";
+}
+
+function collapse() {
+    let container = document.getElementById("header-container");
+    container.style.height = "0%";
+}
